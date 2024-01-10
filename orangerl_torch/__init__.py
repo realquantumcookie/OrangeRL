f4eb7916c1cdc32f70101ce37e8f81e5bbd947e2
Yunhao Cao <realquantumcookie@gmail.com>
2024-01-10 22:43:13 +0800

    Add MLP utility

--- orangerl_torch/__init__.py
+++ orangerl_torch/__init__.py
@@ -2,3 +2,4 @@ from .data import NNBatch, nnbatch_from_transitions, dict_to_tensor_dict, transf
 from .replay_buffer import Storage, ListStorage, TensorStorage, LazyMemmapStorage, LazyTensorStorage, NNReplayBuffer
 from .agent import NNAgent, NNAgentOutput, BatchedNNAgentOutput, Tensor_Or_TensorDict, Tensor_Or_TensorDict_Or_Numpy_Or_Dict
 from .agent_util import NNAgentActionMapper, NNAgentNetworkOutput, NNAgentInputMapper, NNAgentCriticMapper, NNAgentActor, NNAgentActorImpl, NNAgentCritic, NNAgentCriticImpl, BatchedNNCriticOutput
+from .network_util import MLP
---
+++ orangerl_torch/network_util.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+from typing import List, Callable, Optional
+
+def MLP(
+    input_dim: int,
+    output_dim : int,
+    hidden_dims : List[int],
+    activations : Optional[Callable[[int, int], nn.Module]] = lambda x: nn.ReLU(),
+    last_layer_activation : Optional[Callable[[int, int], nn.Module]] = None,
+    use_layer_norm : bool = False,
+    dropout_rate : Optional[float] = None,
+):
+    next_dims = hidden_dims + [output_dim]
+    
+    all_layers = [nn.Flatten()]
+    prev_dim = input_dim
+    for i, next_dim in enumerate(next_dims):
+        all_layers.append(
+            nn.Linear(prev_dim, next_dim)
+        )
+
+        if i < len(next_dims) - 1:
+            if dropout_rate is not None and dropout_rate > 0:
+                all_layers.append(nn.Dropout(dropout_rate))
+            if use_layer_norm:
+                all_layers.append(nn.LayerNorm(next_dim))
+            if activations is not None:
+                all_layers.append(activations(prev_dim, next_dim))
+        else:
+            if last_layer_activation is not None:
+                all_layers.append(last_layer_activation(prev_dim, next_dim))
+        
+        prev_dim = next_dim
+    
+    return nn.Sequential(*all_layers)
